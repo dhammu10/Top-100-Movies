960ab060389e1a48a95ffe228b1853a68a13669e
Dhammdip Modak <124656648+dhammu10@users.noreply.github.com>
2025-07-29 14:28:53 +0530

    Update top_100_movies.py

--- top_100_movies.py
+++ top_100_movies.py
@@ -15,7 +15,6 @@ all_movies = soup.find_all(name="h3", class_="title")
 # print(all_movies)
 movies_title = [movies.getText() for movies in all_movies]
 movies_title.reverse()
-# print(movies_title)
 
 with open("movies.text", mode="w") as file:
     for item in movies_title:
